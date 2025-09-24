abc8a87d6288643b4d47b90e8cb3ed89ceefc1fc
Rajjoshi77 <110289049+Rajjoshi77@users.noreply.github.com>
2025-09-25 01:44:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base:'/Insta-Clone',
-  define: {
-    'process.env': {}, // Ensure compatibility with libraries using process.env
+  base: "/Insta-Clone/",
+  build: {
+    outDir: "dist",
   },
 });
